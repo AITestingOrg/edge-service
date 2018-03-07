faa7290dd437d5bac2b5703a5ec16a516501fb81
Erin Yoo <erinyoo@ufl.edu>
2018-03-07 15:14:07 -0500

    Updated Integration Tests

--- src/test/java/aist/edge/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/aist/edge/edgeservice/EdgeServiceApplicationTests.java
@@ -2,126 +2,72 @@ package aist.edge.edgeservice;
 
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.waiting.HealthChecks;
+
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+
+import org.springframework.http.*;
+import org.springframework.util.*;
+import java.util.*;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = EdgeServiceApplication.class)
+@SpringBootTest(classes = EdgeServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EdgeServiceApplicationTests{
 
 	@ClassRule
 	public static DockerComposeRule docker = DockerComposeRule.
 			builder().pullOnStartup(true)
-			.file("/Users/eriny/microservices/microservices--backing-service--edge-service/src/test/resources/docker-compose.yml")
-			//User Service
-//				.waitingForService("microservice--user-service", HealthChecks.toHaveAllPortsOpen())
-//				.waitingForService("microservice--user-service", HealthChecks.toRespondOverHttp(8091,
-//						(port) -> port.inFormat("http://localhost:8091/")))
+			.file("src/test/resources/docker-compose.yml")
 			//Trip Command Service
-				.waitingForService("mongo", HealthChecks.toHaveAllPortsOpen())
-				.waitingForService("mongo", HealthChecks.toRespondOverHttp(27017,
-						(port) -> port.inFormat("http://localhost:27017")))
+				.waitingForService("trip-management-cmd", HealthChecks.toHaveAllPortsOpen())
+				.waitingForService("trip-management-cmd", HealthChecks.toRespondOverHttp(8080,
+						(port) -> port.inFormat("http://localhost:8092")))
 			//Trip Query Service
-				.waitingForService("rabbitmq", HealthChecks.toHaveAllPortsOpen())
-				.waitingForService("rabbitmq", HealthChecks.toRespondOverHttp(15672,
-						(port) -> port.inFormat("http://localhost:15672/")))
+				.waitingForService("trip-management-query", HealthChecks.toHaveAllPortsOpen())
+				.waitingForService("trip-management-query", HealthChecks.toRespondOverHttp(8080,
+						(port) -> port.inFormat("http://localhost:8093")))
+			//Discovery Service
+				.waitingForService("discovery-service", HealthChecks.toHaveAllPortsOpen())
+				.waitingForService("discovery-service", HealthChecks.toRespondOverHttp(8761,
+						(port) -> port.inFormat("http://localhost:8761")))
 			.build();
 
-	@Autowired
-	private WebApplicationContext context;
-
-	private MockMvc mockMvc;
-
-//	@Before
-//	public void setup() {
-//		mockMvc = MockMvcBuilders
-//			.webAppContextSetup(context)
-//			.apply(springSecurity())
-//			.build();
-//	}
+	private TestRestTemplate restTemplate = new TestRestTemplate();
 
 	@Test
-	public void trip_Post_Request_Test() throws Exception {
-		this.mockMvc = MockMvcBuilders
-				.webAppContextSetup(this.context)
-				.build();
-		this.mockMvc.perform(get("http://localhost:8092/api/trip").contentType(MediaType.APPLICATION_JSON)
-						.content("{ \"originAddress\": \"Somewhere of the origin\", \"destinationAddress\": \"Somewhere destination\"," +
-						" \"userId\": 123e4567-e89b-12d3-a456-426655440000 }"))
-					.andExpect(status().isOk());
+	public void trip_POST_Request_Test() {
+		//given:
+		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+		Map<String, String> map = new HashMap<>();
+		map.put("Content-Type", "application/json");
+		headers.setAll(map);
+		String body = "{ \"originAddress\": \"Somewhere of the origin\", \"destinationAddress\": \"Somewhere destination\"," +
+				 "\"userId\": \"123e4567-e89b-12d3-a456-426655440000\" }";
+		HttpEntity<String> request = new HttpEntity<>(body, headers);
+
+		//when:
+		ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8092/api/trip", request, String.class);
+
+		//then:
+		assertThat(response.getStatusCodeValue()).isEqualTo(200);
 	}
-//
-//	private String getAccessToken(String username, String password) throws Exception {
-//		MockHttpServletResponse response = mockMvc
-//				.perform(post("/oauth/auth/token")
-//						.header("Authorization", "Basic "
-//								+ new String(Base64Utils.encode(("appclient:password")
-//								.getBytes())))
-//						.param("username", username)
-//						.param("password", password)
-//						.param("grant_type", "password"))
-//				.andReturn().getResponse();
-//	}
 
-//	@Ignore
-//	public void testUserAuthorizationService() throws Exception {
-//
-//		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//		params.add("grant_type", "password");
-//		params.add("scope", "webclient");
-//		params.add("username", "user1");
-//		params.add("password", "$2a$04$NX3QTkBJB00upxKeaKqFBeoIVc9JHvwVnj1lItxNphRj34wNx5wlus");
-//
-//		// given:
-//		MockMvcRequestSpecification request = given().params(params);
-//
-//		// when:
-//		ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:8091/oauth/auth/token", String.class);
-//
-//		// then:
-//		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-//	}
+	@Test
+	public void trip_GET_Request_Test() {
+		//given:
 
-//	@Test
-//	public void tripSwagger() throws Exception {
-//		// given:
-//		MockMvcRequestSpecification request = given().header("Content-Type", "application/json");
-//
-//		// when:
-//		ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:8181/trips/", String.class);
-//
-//		// then:
-//		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-//
-//		// and:
-//		assertThat(response.getBody()).contains("http://localhost:8181/trips/");
-//	}
+		//when:
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8093/api/trip", String.class);
 
-//	@Test
-//	public void paymentSwagger() throws Exception {
-//		// given:
-//		MockMvcRequestSpecification request = given().header("Content-Type", "application/json");
-//
-//		// when:
-//		ResponseEntity<String> response = this.testRestTemplate.getForEntity("http://localhost:8082/payments/", String.class);
-//
-//		// then:
-//		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-//
-//		// and:
-//		assertThat(response.getBody()).contains("http://localhost:8082/payments/");
-//	}
-
+		//then:
+		assertThat(response.getStatusCodeValue()).isEqualTo(200);
+	}
 }
