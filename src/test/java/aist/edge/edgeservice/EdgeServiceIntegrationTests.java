27fa8c56dfda97ae5509358e190101dd7e5c19d0
Erin Yoo <erinyoo@ufl.edu>
2018-03-14 10:44:02 -0400

    Updated checkstyle fixes
    Updated travis with docker service

--- src/test/java/aist/edge/edgeservice/EdgeServiceIntegrationTests.java
+++ src/test/java/aist/edge/edgeservice/EdgeServiceIntegrationTests.java
@@ -1,7 +1,11 @@
 package aist.edge.edgeservice;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 import com.palantir.docker.compose.DockerComposeRule;
 import com.palantir.docker.compose.connection.waiting.HealthChecks;
+
+import java.util.*;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,100 +16,89 @@ import org.junit.runner.RunWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.*;
-
-import java.util.*;
-
-import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = EdgeServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EdgeServiceIntegrationTests {
 
-	@ClassRule
-	public static DockerComposeRule docker = DockerComposeRule.
-			builder().pullOnStartup(true)
-			.file("src/test/resources/docker-compose.yml")
-			//User Service
-			.waitingForService("microservice--user-service", HealthChecks.toHaveAllPortsOpen())
-			.waitingForService("microservice--user-service", HealthChecks.toRespondOverHttp(8091,
-					(port) -> port.inFormat("http://localhost:8091")))
-			//Trip Command Service
-				.waitingForService("trip-management-cmd", HealthChecks.toHaveAllPortsOpen())
-				.waitingForService("trip-management-cmd", HealthChecks.toRespondOverHttp(8080,
-						(port) -> port.inFormat("http://localhost:8092")))
-			//Trip Query Service
-				.waitingForService("trip-management-query", HealthChecks.toHaveAllPortsOpen())
-				.waitingForService("trip-management-query", HealthChecks.toRespondOverHttp(8080,
-						(port) -> port.inFormat("http://localhost:8093")))
-			//Discovery Service
-				.waitingForService("discovery-service", HealthChecks.toHaveAllPortsOpen())
-				.waitingForService("discovery-service", HealthChecks.toRespondOverHttp(8761,
-						(port) -> port.inFormat("http://localhost:8761")))
-			.build();
+    @ClassRule
+    public static DockerComposeRule docker = DockerComposeRule.builder().pullOnStartup(true)
+            .file("src/test/resources/docker-compose.yml")
+            .waitingForService("microservice--user-service", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("microservice--user-service", HealthChecks.toRespondOverHttp(8091,
+                (port) -> port.inFormat("http://localhost:8091")))
+            .waitingForService("trip-management-cmd", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("trip-management-cmd", HealthChecks.toRespondOverHttp(8080,
+                (port) -> port.inFormat("http://localhost:8092")))
+            .waitingForService("trip-management-query", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("trip-management-query", HealthChecks.toRespondOverHttp(8080,
+                (port) -> port.inFormat("http://localhost:8093")))
+            .waitingForService("discovery-service", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("discovery-service", HealthChecks.toRespondOverHttp(8761,
+                (port) -> port.inFormat("http://localhost:8761")))
+            .build();
 
-	private TestRestTemplate restTemplate = new TestRestTemplate();
+    private TestRestTemplate restTemplate = new TestRestTemplate();
 
-	private String token;
+    private String token;
 
-	@Before
-	public void setUp() throws JSONException {
-		String plainCreds = "user1:password";
-		byte[] plainCredsBytes = plainCreds.getBytes();
-		byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
-		String base64Creds = new String(base64CredsBytes);
+    @Before
+    public void setUp() throws JSONException {
+        String plainCreds = "user1:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Basic ZWFnbGVleWU6dGhpc2lzc2VjcmV0");
-		headers.add("Content-Type", "application/x-www-form-urlencoded");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
 
-		MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-		parameters.add("username", "eagleeye");
-		parameters.add("secret", "thisissecret");
-		String body = "grant_type=password&scope=webclient&username=user1&password=password";
-		HttpEntity<String> request = new HttpEntity<>(body, headers);
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("username", "eagleeye");
+        parameters.add("secret", "thisissecret");
+        String body = "grant_type=password&scope=webclient&username=user1&password=password";
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
 
-		//when:
-		ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8091/auth/oauth/token", request, String.class, parameters);
+        //when:
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8091/auth/oauth/token", request, String.class, parameters);
 
-		//then:
-		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-		JSONObject json = new JSONObject(response.getBody());
-		token = json.getString("access_token");
-	}
+        //then:
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        JSONObject json = new JSONObject(response.getBody());
+        token = json.getString("access_token");
+    }
 
-	@Test
-	public void tripCommandPOSTRequestSuccess() {
-		//given:
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Bearer " + token);
-		headers.add("Content-Type", "application/json");
+    @Test
+    public void tripCommandPOSTRequestSuccess() {
+        //given:
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
+        headers.add("Content-Type", "application/json");
 
-		String body = "{ \"originAddress\": \"Somewhere of the origin\", \"destinationAddress\": \"Somewhere destination\"," +
-				 "\"userId\": \"123e4567-e89b-12d3-a456-426655440000\" }";
-		HttpEntity<String> request = new HttpEntity<>(body, headers);
+        String body = "{ \"originAddress\": \"Somewhere of the origin\", \"destinationAddress\": "
+                + "\"Somewhere destination\", \"userId\": \"123e4567-e89b-12d3-a456-426655440000\" }";
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
 
-		//when:
-		ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8092/api/trip", request, String.class);
+        //when:
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8092/api/trip", request, String.class);
 
-		//then:
-		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-	}
+        //then:
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+    }
 
-	@Test
-	public void tripQueryGETRequestSuccess() {
-		//given:
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Authorization", "Bearer " + token);
+    @Test
+    public void tripQueryGETRequestSuccess() {
+        //given:
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + token);
 
-		//when:
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8093/api/trips", String.class);
+        //when:
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8093/api/trips", String.class);
 
-		//then:
-		assertThat(response.getStatusCodeValue()).isEqualTo(200);
-	}
+        //then:
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+    }
 }
