30a65f4cbb6d4dfcd900491fc7931f13974189d9
Gabriel Perez Clark <gaboperezclark@gmail.com>
2018-04-16 11:43:18 -0400

    cleaned up changes and removed paths for calc and gmaps

--- src/main/java/aist/edge/edgeservice/EdgeServiceApplication.java
+++ src/main/java/aist/edge/edgeservice/EdgeServiceApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-//@EnableResourceServer
+@EnableResourceServer
 @EnableHystrix
 public class EdgeServiceApplication {
     public static void main(String[] args) {
