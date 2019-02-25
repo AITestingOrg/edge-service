3e47f979660538a5bc2eed2da8ffa772e1007230
Juan <jusoto@gmail.com>
2019-02-25 18:12:59 -0500

    Travis: Pushing files from linux environment

--- src/test/java/aist/edge/edgeservice/EdgeServiceIntegrationTests.java
+++ src/test/java/aist/edge/edgeservice/EdgeServiceIntegrationTests.java
@@ -2,12 +2,14 @@ package aist.edge.edgeservice;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.DockerPort;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Base64;
-import java.util.List;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,11 +29,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import com.palantir.docker.compose.DockerComposeRule;
-import com.palantir.docker.compose.connection.Container;
-import com.palantir.docker.compose.connection.DockerPort;
-import com.palantir.docker.compose.connection.waiting.HealthChecks;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = EdgeServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EdgeServiceIntegrationTests {
@@ -45,7 +42,7 @@ public class EdgeServiceIntegrationTests {
     private static String gmapsAdapterURL;
     private static String calculationServiceURL;
 
-  //Wait for all services to have ports open
+    // Wait for all services to have ports open
     @ClassRule
     public static DockerComposeRule docker = DockerComposeRule.builder().pullOnStartup(true)
             .file("src/test/resources/docker-compose.yml")
@@ -57,48 +54,41 @@ public class EdgeServiceIntegrationTests {
             .waitingForService("gmapsadapter", HealthChecks.toHaveAllPortsOpen())
             .waitingForService("calculationservice", HealthChecks.toHaveAllPortsOpen())
             .waitingForService("discoveryservice", HealthChecks.toHaveAllPortsOpen())
-//            .waitingForService("discoveryservice", HealthChecks.toRespondOverHttp(8761,
-//                (port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT")))
+            .waitingForService("discoveryservice",
+                HealthChecks.toRespondOverHttp(8761, (port) -> port.inFormat("http://$HOST:$EXTERNAL_PORT")))
             .build();
 
-    //Get IP addresses and ports to run tests on
+    // Get IP addresses and ports to run tests on
     @BeforeClass
     public static void initialize() {
         LOG.info("Initializing ports from Docker");
-        DockerPort tripManagementCommand = docker.containers().container("tripmanagementcmd")
-                .port(8080);
+        DockerPort tripManagementCommand = docker.containers().container("tripmanagementcmd").port(8080);
         tripCommandURL = String.format("http://%s:%s", tripManagementCommand.getIp(),
                 tripManagementCommand.getExternalPort());
         LOG.info("Trip Command url found: " + tripCommandURL);
 
-        DockerPort tripManagementQuery = docker.containers().container("tripmanagementquery")
-                .port(8080);
+        DockerPort tripManagementQuery = docker.containers().container("tripmanagementquery").port(8080);
         tripQueryURL = String.format("http://%s:%s", tripManagementQuery.getIp(),
                 tripManagementQuery.getExternalPort());
         LOG.info("Trip Query url found: " + tripQueryURL);
 
-        DockerPort gmapsAdapter = docker.containers().container("gmapsadapter")
-                .port(8080);
-        gmapsAdapterURL = String.format("http://%s:%s", gmapsAdapter.getIp(),
-                gmapsAdapter.getExternalPort());
+        DockerPort gmapsAdapter = docker.containers().container("gmapsadapter").port(8080);
+        gmapsAdapterURL = String.format("http://%s:%s", gmapsAdapter.getIp(), gmapsAdapter.getExternalPort());
         LOG.info("Gmaps Adapter url found: " + gmapsAdapterURL);
 
-        DockerPort calculationService = docker.containers().container("calculationservice")
-                .port(8080);
+        DockerPort calculationService = docker.containers().container("calculationservice").port(8080);
         calculationServiceURL = String.format("http://%s:%s", calculationService.getIp(),
                 calculationService.getExternalPort());
-        while (!docker.containers().container("calculationservice").portIsListeningOnHttp(8080,
-            (port) -> port.inFormat(calculationServiceURL)).succeeded()) {
+        while (!docker.containers().container("calculationservice")
+                .portIsListeningOnHttp(8080, (port) -> port.inFormat(calculationServiceURL)).succeeded()) {
             LOG.info("Waiting for calculation service to respond over HTTP");
         }
         LOG.info("Calculation Service url found: " + calculationServiceURL);
 
-        DockerPort userService = docker.containers().container("userservice")
-                .port(8080);
-        userServiceURL = String.format("http://%s:%s", userService.getIp(),
-                userService.getExternalPort());
-        while (!docker.containers().container("userservice").portIsListeningOnHttp(8080,
-            (port) -> port.inFormat(userServiceURL)).succeeded()) {
+        DockerPort userService = docker.containers().container("userservice").port(8080);
+        userServiceURL = String.format("http://%s:%s", userService.getIp(), userService.getExternalPort());
+        while (!docker.containers().container("userservice")
+                .portIsListeningOnHttp(8080, (port) -> port.inFormat(userServiceURL)).succeeded()) {
             LOG.info("Waiting for user service to respond over HTTP");
         }
         LOG.info("User Service url found: " + userServiceURL);
@@ -126,70 +116,8 @@ public class EdgeServiceIntegrationTests {
         HttpEntity<String> request = new HttpEntity<>(body, headers);
 
         // when:
-        ResponseEntity<String> response;
-
-        //////////////////////////////////////////////
-
-         //List<String> containerList = new ArrayList<String>();
-         Runtime rt = Runtime.getRuntime();
-        
-         String[] commands = { "netstat", "-an" };
-         Process proc = rt.exec(commands);
-        
-         BufferedReader stdInput = new BufferedReader(new
-         InputStreamReader(proc.getInputStream()));
-        
-         BufferedReader stdError = new BufferedReader(new
-         InputStreamReader(proc.getErrorStream()));
-        
-         // read the output from the command
-         System.out.println("****** Here is the standard output of the command:\n");
-         String s = null;
-         while ((s = stdInput.readLine()) != null) {
-         // containerList.add(s);
-         System.out.println(s);
-         LOG.info(s);
-         }
-        
-         // read any errors from the attempted command
-         System.out.println("****** Here is the standard error of the command (if any):\n");
-         while ((s = stdError.readLine()) != null) {
-         System.out.println(s);
-         LOG.info(s);
-         }
-        //
-        // for (String containerId : containerList) {
-        //
-        // String[] commands1 = { "docker", "inspect", containerId };
-        // Process proc1 = rt.exec(commands1);
-        //
-        // BufferedReader stdInput1 = new BufferedReader(new
-        // InputStreamReader(proc1.getInputStream()));
-        //
-        // BufferedReader stdError1 = new BufferedReader(new
-        // InputStreamReader(proc1.getErrorStream()));
-        //
-        // // read the output from the command
-        // System.out.println("Here is the standard output of the command:\n");
-        // String s1 = null;
-        // while ((s1 = stdInput1.readLine()) != null) {
-        // System.out.println(s1);
-        // LOG.info(s1);
-        // }
-        //
-        // // read any errors from the attempted command
-        // System.out.println("Here is the standard error of the command (if
-        // any):\n");
-        // while ((s1 = stdError1.readLine()) != null) {
-        // System.out.println(s1);
-        // LOG.info(s1);
-        // }
-        // }
-        //////////////////////////////////////////////
-
-        LOG.info(String.format("User Service - Trying url: %s", userServiceURL));
-        response = restTemplate.postForEntity(userServiceURL + "/auth/oauth/token", request, String.class, parameters);
-        LOG.info("Request succeded");
+        ResponseEntity<String> response = restTemplate.postForEntity(userServiceURL + "/auth/oauth/token", request,
+                String.class, parameters);
 
         // then:
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
